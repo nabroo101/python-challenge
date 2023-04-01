08e1443556b3371a38d3516b9bd497e8472922fd
nabroo101 <elnabawy101@gmail.com>
2023-04-01 01:55:55 -0400

    calculate total number and net total

--- PyBank/Resources/budget_data_analysis.py
+++ PyBank/Resources/budget_data_analysis.py
@@ -1,22 +1,32 @@
 import os
 import csv
-
+print('Financial Analysis')
+print('----------------------------')
 # assigning the file path to a variable
-#budget_csv_path = os.path.join('.', 'budget_data.csv')
-budget_csv_path = './Resources/budget_data.csv'
-print(budget_csv_path)
+budget_csv_path = os.path.join('.', 'budget_data.csv')
+#budget_csv_path = './Resources/budget_data.csv'
+#print(budget_csv_path)
 
-with open(budget_csv_path, 'r') as csv_file:
+with open(budget_csv_path, 'r', encoding="utf-8") as csv_file:
     row_csv = csv_file.read()
-    print(row_csv)
+    #print(row_csv)
 #    print(type(row_csv))
 
 with open(budget_csv_path, 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    print(csv_reader)
+    #print(csv_reader)
 # if data has header which it does
     header_csv = next(csv_reader)
-    print(f'CSV Headers: {header_csv}')
+    #print(f'CSV Headers: {header_csv}')
 
-    for row in csv_reader:
-        print(row)
+    rows = list(csv_reader)
+    total_number_of_months = len(rows)
+
+    print(f'Total Months: {total_number_of_months}')
+    #print(rows)
+net_total = 0
+for sub_list_profit_losses in rows:
+    profit_loss = int(sub_list_profit_losses[1])
+    net_total += profit_loss
+print(f'Total: ${net_total}')
+        
